a46dcfe461f6dedb4c4d062b78f97ee8414546ef
Victor Salgado <odaglas1870@gmail.com>
2018-10-19 12:21:42 -0500

    Inicio secion 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+// Modulo de rutas de navegacion entre las paginas //
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafilioComponent } from './pages/portafilio/portafilio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  {path: 'home', component: PortafilioComponent }, // cuando esta paht en blanco ir portafiliocomponet.html //
+  {path: 'about', component: AboutComponent }, // redireccionar al aboutcomponet.html //
+  {path: 'item', component: ItemComponent}, // redireccionar al itemcomponet.html //
+  {path: '**', pathMatch: 'full' , redirectTo: 'home'} // redireccionar a portafilocomponet.html //
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas de la aplicacion
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafilioComponent } from './pages/portafilio/portafilio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafilioComponent
+    PortafilioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
