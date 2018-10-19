92aacc92d5b292b7fd7babf573ebb61cab26846e
Victor Salgado <odaglas1870@gmail.com>
2018-10-19 09:40:04 -0500

    Templet listo sin rutas

---
+++ src/app/pages/portafilio/portafilio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portafilio',
+  templateUrl: './portafilio.component.html',
+  styleUrls: ['./portafilio.component.css']
+})
+export class PortafilioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
